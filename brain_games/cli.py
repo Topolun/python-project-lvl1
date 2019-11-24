9dc9729f3209760c8535cf94a33961988961d689
Topolun <vik83tu@yandex.ru>
2019-11-24 15:15:24 +0400

    исправлены ошибки после случайного "git pull"

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,5 +2,5 @@ import prompt
 
 
 def run():
-  name = prompt.string('May I have your name? ')
-  print('Hello, ', name + '!')
+    name = prompt.string('May I have your name? ')
+    print('Hello, ', name + '!')
