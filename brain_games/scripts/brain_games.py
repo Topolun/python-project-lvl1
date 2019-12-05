5a3b4bb67c6b512277e65246b29c682b9662b54f
Topolun <vik83tu@yandex.ru>
2019-12-05 16:03:42 +0400

    Исправлена функция main()

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,8 +3,8 @@ from brain_games.cli import run
 
 def main():
     print('Welcome to the Brain Games! \nSay "Yes" if number even otherwise answer "no".')
+    run()
 
 
 if __name__ == '__main__':
     main()
-    run()
