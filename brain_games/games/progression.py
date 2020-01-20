b7bdb7b17f00b1f72f8f4f7b63d7352480467b8c
Topolun <vik83tu@yandex.ru>
2020-01-20 21:17:54 +0400

    Доработана игра "прогрессия"

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,23 +2,17 @@ from random import randint
 
 
 def recieve_data_for_round():
-    progression = randint(1, 100)
+    first_number = randint(1, 100)
     progression_output = ''
     step = randint(1, 10)
     unknown_number = randint(0, 9)
     correct_answer = 0
     for i in range(10):
-        '''
-        В случае 'step * i' не учитывается случай, когда скрытым числом
-        является 0, либо все прогрессии начинаются с 0
-        '''
-        if i != 0:
-            progression = progression + step
         if i == unknown_number:
             progression_output = progression_output + '.. '
-            correct_answer = progression
+            correct_answer = first_number + step * i
         else:
-            progression_output = progression_output + str(progression) + ' '
+            progression_output += str(first_number + step * i) + ' '
     return str(correct_answer), progression_output
 
 
